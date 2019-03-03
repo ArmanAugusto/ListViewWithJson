7e770dc1f38cd8bf35db7eae8fe73ade3502d710
Arman Augusto <arman.augusto@gmail.com>
2019-03-02 22:00:06 -0800

    initial commit

---
+++ ListViewWithJson/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ListViewWithJson.Models;
+using Xamarin.Forms;
+
+namespace ListViewWithJson
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<ProductData> productDataFromJson;
+        string fileName = "ListViewWithJson.products.json";
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ReadInJsonFile();
+        }
+
+        private void ReadInJsonFile()
+        {
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream(fileName);
+
+            using (var reader = new StreamReader(stream))
+            {
+                var jsonAsString = reader.ReadToEnd();
+                productDataFromJson = JsonConvert.DeserializeObject<List<ProductData>>(jsonAsString);
+            }
+            ListViewWithCustomCells.ItemsSource = new ObservableCollection<ProductData>(productDataFromJson);
+        }
+
+        async void Handle_ContextMenuMoreButton(object sender, System.EventArgs e)
+        {
+            var menuItem = (MenuItem)sender;
+            var itemClicked = menuItem.CommandParameter as ProductData;
+            await Navigation.PushAsync(new MoreInfoPage(itemClicked));
+        }
+    }
+}
---
+++ ListViewWithJson/MoreInfoPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using ListViewWithJson.Models;
+
+using Xamarin.Forms;
+
+namespace ListViewWithJson
+{
+    public partial class MoreInfoPage : ContentPage
+    {
+        public MoreInfoPage(Models.ProductData currentItem)
+        {
+            InitializeComponent();
+            ProductName.Text = "Item Name: " + currentItem.ProductName;
+            ProductImage.Source = currentItem.ImageUrl;
+            ProductDescription.Text = "Item Description: " + currentItem.Description;
+            ProductPrice.Text = "Price: $" + currentItem.Price;
+            ProductRating.Text = "Rating: " + currentItem.StarRating;
+
+        }
+    }
+}
